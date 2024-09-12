a6cb1695c540cdf9b5715dc931b08fe63ba89662
Jayden Houghton <jayden-houghton@live.nmit.ac.nz>
2024-09-12 15:37:39 +1200

    Fix mern jwt bugs (working)

--- jwt_mern/backend/app/middlewares/authJwt.js
+++ jwt_mern/backend/app/middlewares/authJwt.js
@@ -25,22 +25,8 @@ verifyToken = (req, res, next) => {
 };
 
 isAdmin = (req, res, next) => {
-  User.findById(req.userId).exec((err, user) => {
-    if (err) {
-      res.status(500).send({ message: err });
-      return;
-    }
-
-    Role.find(
-      {
-        _id: { $in: user.roles }
-      },
-      (err, roles) => {
-        if (err) {
-          res.status(500).send({ message: err });
-          return;
-        }
-
+  User.findById(req.userId).then((user) => {
+    Role.find({_id: { $in: user.roles }}).then((roles) => {
         for (let i = 0; i < roles.length; i++) {
           if (roles[i].name === "admin") {
             next();
@@ -51,27 +37,19 @@ isAdmin = (req, res, next) => {
         res.status(403).send({ message: "Require Admin Role!" });
         return;
       }
-    );
+    ).catch((err) => {
+      res.status(500).send({ message: err });
+      return;
+    });
+  }).catch((err) => {
+    res.status(500).send({ message: err });
+    return;
   });
 };
 
 isModerator = (req, res, next) => {
-  User.findById(req.userId).exec((err, user) => {
-    if (err) {
-      res.status(500).send({ message: err });
-      return;
-    }
-
-    Role.find(
-      {
-        _id: { $in: user.roles }
-      },
-      (err, roles) => {
-        if (err) {
-          res.status(500).send({ message: err });
-          return;
-        }
-
+  User.findById(req.userId).then((user) => {
+    Role.find({_id: { $in: user.roles }}).then((roles) => {
         for (let i = 0; i < roles.length; i++) {
           if (roles[i].name === "moderator") {
             next();
@@ -82,7 +60,13 @@ isModerator = (req, res, next) => {
         res.status(403).send({ message: "Require Moderator Role!" });
         return;
       }
-    );
+    ).catch((err) => {
+      res.status(500).send({ message: err });
+      return;
+    });
+  }).catch((err) => {
+    res.status(500).send({ message: err });
+    return;
   });
 };
 
